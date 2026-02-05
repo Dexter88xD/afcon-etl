deca61372e0c4b00ab58fae1666f2a0c285fc476
Dexter88xD <soulaymanehamdan2@gmail.com>
2026-02-05 11:53:17 +0100

    epic 2: etl - transform is done

---
+++ afcon_etl/transform/transform.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def transform_data(df):
+
+	clean_df = df.copy()
+
+	clean_df["Date"] = pd.to_datetime(clean_df["Date"], format="%d/%m/%Y %H:%M", errors="coerce")
+
+	clean_df["year"] = clean_df["Date"].dt.year
+	clean_df["month"] = clean_df["Date"].dt.month 
+	clean_df["day"] = clean_df["Date"].dt.day
+	clean_df["time"] = clean_df["Date"].dt.time
+
+	clean_df = clean_df.drop(columns=["Date"])
+
+	clean_df = clean_df.fillna(value=None)
+
+	clean_df = clean_df.drop_duplicates()
+
+	return clean_df
